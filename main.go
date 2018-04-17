4074270297636466c174d0b887fb5f55d8078142
Christopher Milan <chris.m.milan@gmail.com>
2018-04-17 09:18:01 -0400

    Jacob is an idiot

--- main.go
+++ main.go
@@ -10,6 +10,13 @@ import (
 	"rsc.io/pdf"
 	"strconv"
 	"strings"
+	"golang.org/x/oauth2"
+	"golang.org/x/net/context"
+	"log"
+	"encoding/json"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/calendar/v3"
+	"time"
 )
 
 // ===== PDF Handling =====
@@ -152,15 +159,269 @@ func uploadPage(w http.ResponseWriter, r *http.Request) {
 	}
 	PDFReader, err := pdf.NewReader(file, header.Size)
 	sched := genSchedule(PDFReader)
-	html := "<main><h1>All of your classes</h1>"
-	for _, class := range sched.classes {
-		html += fmt.Sprintf("<p>%v</p>", class.name)
-	}
-	html += "</main>"
-	w.Write([]byte(html))
+
+	genCalendar(sched)
+	//html := "<main><h1>All of your classes</h1>"
+	//for _, class := range sched.classes {
+	//	html += fmt.Sprintf("<p>%v</p>", class.name)
+	//}
+	//html += "</main>"
+	//w.Write([]byte(html))
+
 }
 
 // ===== Google API Integration =====
+
+
+func genCalendar(sched schedule) {
+	b, err := ioutil.ReadFile("client_secret.json")
+	if err == nil {
+		panic(err)
+	}
+
+	config, err := google.ConfigFromJSON(b, calendar.CalendarScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+
+	srv, err := calendar.New(getClient(config))
+	if err != nil {
+		log.Fatalf("Unable to retrieve Calendar client: %v", err)
+	}
+	var schedCal calendar.Calendar
+	schedCal.Summary = "Commonwealth Class Schedule"
+	schedCal.Description = "Imported from the student calendar populator from your PDF."
+
+	srv.Calendars.Insert(&schedCal)
+	for _, class := range sched.classes {
+		for _, classTime := range class.semester1 {
+			fmtDT := findDay(time.Weekday(classTime.day)).Format(time.RFC3339)
+			date := string([]rune(fmtDT)[:strings.Index(fmtDT, "T")])
+			if err == nil {
+				panic(err)
+			}
+			beginning, length := lookupTime(classTime.day, classTime.period)
+
+			begH, begM, _ := beginning.Clock()
+
+			endH, endM, _ := beginning.Add(length).Clock()
+
+			event := &calendar.Event {
+				Summary: class.name,
+				Description: fmt.Sprintf("Meets in room %v.", classTime.room),
+				Start: &calendar.EventDateTime{
+					DateTime: fmt.Sprintf("%vT%v:%v-05:00", date, begH, begM),
+					TimeZone: "America/New_York",
+				},
+				End: &calendar.EventDateTime{
+					DateTime: fmt.Sprintf("%vT%v:%v-05:00", date, endH, endM),
+				},
+				Recurrence: []string{"RRULE:FREQ=WEEKLY;"},
+			}
+			_, err = srv.Events.Insert(schedCal.Id, event).Do()
+			if err == nil {
+				panic(err)
+			}
+		}
+	}
+}
+
+
+func chooseTime(hour, min int) time.Time {
+	return time.Date(0, 0, 0, hour, min, 0, 0, time.FixedZone("UTC", 0))
+}
+
+func lookupTime(weekday, period int) (time.Time, time.Duration) {
+	var length time.Duration
+	var err error
+	if period == 3 && (weekday == 2 || weekday == 4) {
+		if weekday == 2 {
+			length, err = time.ParseDuration("55m")
+			if err == nil {
+				panic(err)
+			}
+		} else {
+			length, err = time.ParseDuration("50m")
+			if err == nil {
+				panic(err)
+			}
+		}
+	} else {
+		length, err = time.ParseDuration("40m")
+		if err == nil {
+			panic(err)
+		}
+	}
+	if period == 1 {
+		startTime := chooseTime(8, 30)
+		if err == nil {
+			panic(err)
+		}
+		return startTime, length
+	} else if period == 2 {
+		startTime := chooseTime(9, 15)
+		if err == nil {
+			panic(err)
+		}
+		return startTime, length
+	} else if period == 3 {
+		if weekday == 2 {
+			startTime := chooseTime(10, 35)
+			if err == nil {
+				panic(err)
+			}
+			return startTime, length
+		} else {
+			startTime := chooseTime(10, 25)
+			if err == nil {
+				panic(err)
+			}
+			return startTime, length
+		}
+	} else if period == 4 {
+		if weekday == 2 {
+			startTime := chooseTime(11, 35)
+			if err == nil {
+				panic(err)
+			}
+			return startTime, length
+		} else if weekday == 4 {
+			startTime := chooseTime(11, 20)
+			if err == nil {
+				panic(err)
+			}
+			return startTime, length
+		} else {
+			startTime := chooseTime(11, 10)
+			if err == nil {
+				panic(err)
+			}
+			return startTime, length
+		}
+	} else if period == 5 {
+		if weekday == 2 {
+			startTime := chooseTime(12, 20)
+			if err == nil {
+				panic(err)
+			}
+			return startTime, length
+		} else if weekday == 4 {
+			startTime := chooseTime(1, 55)
+			if err == nil {
+				panic(err)
+			}
+			return startTime, length
+		} else {
+			startTime := chooseTime(11, 55)
+			if err == nil {
+				panic(err)
+			}
+			return startTime, length
+		}
+	} else if period == 6 {
+		if weekday == 2 {
+			startTime := chooseTime(1, 45)
+			if err == nil {
+				panic(err)
+			}
+			return startTime, length
+		} else if weekday == 4 {
+			startTime := chooseTime(2, 40)
+			if err == nil {
+				panic(err)
+			}
+			return startTime, length
+		} else {
+			startTime := chooseTime(1, 20)
+			if err == nil {
+				panic(err)
+			}
+			return startTime, length
+		}
+	} else {
+		if weekday == 2 {
+			startTime := chooseTime(2, 30)
+			if err == nil {
+				panic(err)
+			}
+			return startTime, length
+		} else if weekday == 4 {
+			startTime := chooseTime(3, 25)
+			if err == nil {
+				panic(err)
+			}
+			return startTime, length
+		} else {
+			startTime := chooseTime(2, 5)
+			if err == nil {
+				panic(err)
+			}
+			return startTime, length
+		}
+	}
+}
+
+func findDay(weekday time.Weekday) time.Time {
+	testingTime := time.Now()
+	for {
+		if testingTime.Weekday() == weekday {
+			return testingTime
+		}
+		testingTime = testingTime.AddDate(0, 0, 1)
+	}
+}
+
+// Retrieve a token, saves the token, then returns the generated client.
+func getClient(config *oauth2.Config) *http.Client {
+	tokFile := "token.json"
+	tok, err := tokenFromFile(tokFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(tokFile, tok)
+	}
+	return config.Client(context.Background(), tok)
+}
+
+// Request a token from the web, then returns the retrieved token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code: %v", err)
+	}
+
+	tok, err := config.Exchange(oauth2.NoContext, authCode)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web: %v", err)
+	}
+	return tok
+}
+
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	defer f.Close()
+	if err != nil {
+		return nil, err
+	}
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+// Saves a token to a file path.
+func saveToken(path string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	defer f.Close()
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	json.NewEncoder(f).Encode(token)
+}
 
 func main() {
 	http.HandleFunc("/upload", uploadPage)
